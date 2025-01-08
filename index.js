80b30ad3eac6d82ab1265348dad640fcaa758a26
Ahamefule Onyegbule <aonyeobi@gmail.com>
2025-01-08 15:31:42 -0500

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(array, driverName){
+    return array.filter((driver) => {
+        return driver.toLowerCase() === driverName.toLowerCase()
+    })
+}
+
+function fuzzyMatch(array, string){
+    return array.filter((car) => {
+        return car[0] === string[0]
+    })
+}
+
+function matchName(array, string){
+    return array.filter(object => {
+        for(const key in object){
+            return object[key] === string
+        }
+    })
+}
